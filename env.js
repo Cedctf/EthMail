e19313f7dfd66965308f60ee3d68fd4933ceb31e
Jay-366 <loyqunjie@gmail.com>
2025-04-05 19:46:16 +0800

    debug on the json file for the compoonent

---
+++ env.js
@@ -0,0 +1,5 @@
+// Client-side environment variables
+export const clientEnv = {
+  GOOGLE_CLIENT_ID: "126734007041-d2vavanache4294rl4s2o2c6gku50vc0.apps.googleusercontent.com",
+  GOOGLE_REDIRECT_URI: "http://localhost:3000/auth/callback"
+}; 
