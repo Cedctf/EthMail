c183456ca0ba00dd89bb5d75d9cfd209ba40c381
Jay-366 <loyqunjie@gmail.com>
2025-04-05 19:45:08 +0800

    layout and the route

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { AuthProvider } from "@/components/auth-provider"
+import { usePathname } from "next/navigation"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const pathname = usePathname()
+  const isLandingPage = pathname === '/landing'
+
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        {isLandingPage ? (
+          children
+        ) : (
+          <AuthProvider>{children}</AuthProvider>
+        )}
+      </body>
+    </html>
+  )
+}
